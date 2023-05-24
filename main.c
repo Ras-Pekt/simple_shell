0666cf4cb85b66e00b2ae83a829e7da0b4565814
Wanjang'i Gituku <wanjangi.gituku@gmail.com>
2023-05-24 13:08:08 +0300

    temp revision code

--- main.c
+++ main.c
@@ -1,120 +1,32 @@
 #include "shell.h"
 
 /**
-* main - Entry point
-* @ac: argument count
-* @av: argument vector
-* @env: environ array
-* Return: 0 on success
-*/
-int main(int ac, char **av, char **env)
-{
-	char *str;
-	int tok_return;
-	char *argv[1024];
-	static int count = 1;
+ * clear_array - free an array
+ * @array: array to free
+ */
 
-	(void)ac;
-
-	while (1)
-	{
-		int *ptr_count = &count;
-
-		prompt();
-
-		str = _read();
-		if (str == NULL)
-		{
-			if (isatty(STDIN_FILENO))
-				_puts("\n");
-			free(str);
-			exit(EXIT_SUCCESS);
-		}
-
-		tok_return = splitString(str, argv);
-		if (tok_return > 0)
-		{
-			if (str_cmp(argv[0], "exit") == 0)
-				_argExit(av[0], count, str, argv);
-			else if (str_cmp(argv[0], "env") == 0)
-				_env(env);
-			else
-				_execute(argv, count, av[0], env);
-		}
-		free(str);
-		*ptr_count = count + 1;
-	}
-
-	return (0);
-}
-/**
-* _puts - writes a string to stdout
-* @str: The string to print
-* Return: number of elements printed
-*/
-int _puts(char *str)
+void clear_array(char **array)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-		;
-
-	write(STDOUT_FILENO, str, i);
-
-	return (i);
-}
-/**
-* _putint - prints any integer using _puts
-* @n: input integer
-* Return: nothing
-*/
-void _putint(int n)
-{
-	char temp, str[10];
-	int len = 0, i = 0, digit, j;
-
-	while (n > 0)
+	for (i = 0; array[i]; i++)
 	{
-		digit = n % 10;
-		str[len] = digit + '0';
-		n /= 10;
-		len++;
+		free(array[i]);
+		array[i] = NULL;
 	}
-	str[len] = '\0';
+	free(array);
+	array = NULL;
+}
 
-	j = len - 1;
-	while (i < j)
-	{
-		temp = str[i];
-		str[i] = str[j];
-		str[j] = temp;
-		i++;
-		j--;
-	}
-	_puts(str);
-}
 /**
-* _perror - prints error of commands not found
-* @av: name of the shell
-* @count: shell iteration count
-* @tokenArray: array containing user tokenized string
-* Return: nothing
-*/
-void _perror(char *av, int count, char **tokenArray)
+ * main - function
+ * Return: 0
+ */
+
+int main(void)
 {
-	_puts(av);
-	_puts(": ");
-	_putint(count);
-	_puts(": ");
-	_puts(tokenArray[0]);
-	_puts(": not found\n");
-}
-/**
-* prompt - displays the prompt
-* Return: nothing
-*/
-void prompt(void)
-{
-	if (isatty(STDIN_FILENO))
-		_puts("vsh$ ");
+	signal(SIGINT, snal_H);
+	lup(isatty(STDIN_FILENO));
+
+	return (0);
 }
--- shell.h
+++ shell.h
@@ -1,48 +1,38 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
 #include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <signal.h>
 
 extern char **environ;
 
-/**
-* struct list_s - singly linked list
-* @str: string - (malloc'ed string)
-* @next: points to the next node
-*
-* Description: singly linked list node structure
-*/
-typedef struct list_s
-{
-	char *str;
-	struct list_s *next;
-} list_t;
+void _path(char **cmd);
+ssize_t str_len(char *s);
+int str_cmp(char *st1, char *st2);
+char *str_cpy(char *destn, char *src);
+char *app_string(char *destn, const char *src);
+void depart(char **cmd, char *ln);
+int str_cmp2(const char *st1, const char *st2);
+int atoi_(char *s);
+void envr(void);
+int put_char(char c);
+void snal_H(__attribute__((unused)) int s_num);
+int str_len2(char *s);
+void clear_array(char **array);
+void put_char2(char *c);
+void lup(int inpt);
+void ex_cd(char **cmd);
+void *rea_lloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char *str_dup(char *str);
+char **str_splt(char *ln, char *delim);
+char *strn_cpy(char *dest, const char *src, size_t n);
+ssize_t my_puts(char *s);
 
-int _puts(char *str);
-void _putint(int n);
-void _perror(char *av, int count, char **tokenArray);
-void prompt(void);
-
-char *_read();
-int splitString(char *str, char **tokenArray);
-int _execute(char **tokenArray, int count, char *av, char **env);
-int _env(char **env);
-void _argExit(char *av, int count, char *str, char **tokenArray);
-
-char *_getenv(const char *name);
-list_t *_path(void);
-char *_getpath(char *str);
-list_t *add_node(list_t **head, const char *str);
-void free_list(list_t *head);
-
-int str_cmp(char *s1, char *s2);
-char *str_dup(const char *str);
-char *str_cat(char *dest, char *src);
-int _atoi(char *str);
 
 #endif
