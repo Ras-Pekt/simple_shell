e0fa94d73283d25e3a09d47780d863f35fea471f
Lavender <lavenderanyango9@gmail.com>
2023-05-25 00:39:03 -0700

    updating cd

--- builtins.c
+++ builtins.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * _setenv - implements setenv builtin command
- * @tokenArray: array of user input
- * Return: void
- */
+* _setenv - implements setenv builtin command
+* @tokenArray: array of user input
+* Return: void
+*/
 void _setenv(char **tokenArray)
 {
 	int n;
@@ -19,10 +19,10 @@ void _setenv(char **tokenArray)
 		_puterror("setenv: failed to set environment variable\n");
 }
 /**
- * _unsetenv - implements unsetenv builtin command
- * @tokenArray: array of user input
- * Return: void
- */
+* _unsetenv - implements unsetenv builtin command
+* @tokenArray: array of user input
+* Return: void
+*/
 void _unsetenv(char **tokenArray)
 {
 	if (tokenArray[1] == NULL)
@@ -31,46 +31,53 @@ void _unsetenv(char **tokenArray)
 		_puterror("unsetenv: failed to unset environment variable\n");
 }
 /**
- * _chdir - implements cd builtin command
- * @tokenArray: array of user input
- * Return: void
- */
+* _chdir - implements cd builtin command
+* @tokenArray: array of user input
+* Return: void
+*/
 void _chdir(char **tokenArray)
 {
 	char path[128];
 	/*the current pwd is the nxtOldPwd*/
 	char *nxtOldPwd = getcwd(path, 128);
+	char *home = _getenv("HOME");
+	char *oldpwd = _getenv("OLDPWD");
+	char *new_wd = NULL;
+
+	if (home == NULL)
+	{
+		home = "/";
+	}
 
 	if (tokenArray[1] == NULL)
 	{
-		if (chdir(_getenv("HOME")) != 0)
+		if (chdir(home) != 0)
 			perror("cd");
 		setenv("OLDPWD", nxtOldPwd, 1);
 		setenv("PWD", getcwd(path, 128), 1);
 	}
 	else if (str_cmp(tokenArray[1], "-") == 0)
 	{
-		if (_getenv("OLDPWD") == NULL)
-			_puterror(": cd: OLDPWD not set\n");
-		else if (chdir(_getenv("OLDPWD")) != 0)
+		if (oldpwd == NULL)
+		{
+			oldpwd = "/";
+		}
+
+		else if (chdir(oldpwd) != 0)
 			perror("cd");
 
 		setenv("OLDPWD", nxtOldPwd, 1);
 		setenv("PWD", getcwd(path, 128), 1);
-	}
-	else
-	{
-		if (chdir(tokenArray[1]) != 0)
-			perror("cd");
-		setenv("OLDPWD", nxtOldPwd, 1);
-		setenv("PWD", getcwd(path, 128), 1);
+
+		new_wd = getcwd(path, 128);
+		_puts(new_wd);
 	}
 }
 /**
- * _puterror - writes a string to stderr
- * @str: The string to print
- * Return: number of elements printed
- */
+* _puterror - writes a string to stderr
+* @str: The string to print
+* Return: number of elements printed
+*/
 int _puterror(char *str)
 {
 	int i = 0;
